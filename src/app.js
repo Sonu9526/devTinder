729d690b8a35f693e34d443efa826962186d2051
Sonu S Nair <sonu.nair@gmail.com>
2026-01-17 23:46:24 +0530

    auth

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const exp = require("express")
 const app = exp()
 
-app.use("/", (req, res) => {
-    res.send("Hello from home page")
+app.use("/user/2", (req, res) => {
+    res.send("Hello from home/2 page")
 })
+
 app.use("/user", (req, res) => {
     res.send("Hello from user page")
+})
+
+app.use("/", (req, res) => {
+    res.send("Hello from home page")
 })
 
 app.listen(1111, () => {
